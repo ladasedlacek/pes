72b739a06b0cf6b18479c0261d994b12eb9db0ae
ladasedlacek <sedlacek.lada@gmail.com>
2022-03-20 14:41:01 +0100

    Created breed section

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
     // desktop layout swticher
     $("#layout_switch button").on("click", function () {
-        $("#testLayout").removeClass("layout-0").removeClass("layout-1"), $("body").addClass("mobile");
+        $("#testLayout").removeClass("layout-0").removeClass("layout-1"), 
+        $("body").addClass("mobile");
     }),
-
     $("#layout_switch button:nth-child(2)").on("click", function () {
-        $("#testLayout").addClass("layout-0").css({'width':'1035px', 'margin':'0 auto'}), $("body.mobile").removeClass("mobile");
+        $("#testLayout").addClass("layout-0").css({'width':'1035px', 'margin':'0 auto'}), 
+        $("body.mobile").removeClass("mobile");
     }),
     $("#layout_switch button:nth-child(3)").on("click", function () {
-        $("#testLayout").addClass("layout-1").css({'width':'1310px', 'margin':'0 auto'}), $("body.mobile").removeClass("mobile");
+        $("#testLayout").addClass("layout-1").css({'width':'1310px', 'margin':'0 auto'}), 
+        $("body.mobile").removeClass("mobile");
     }),
 
     // tabs switcher
